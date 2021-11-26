05ab3e35a1995e235d16df34935c07a2b12735e9
brahmakulkarni <brahma.kulkarni@iiitb.org>
2021-11-26 11:24:20 +0530

    Added post-user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_124356) do
+ActiveRecord::Schema.define(version: 2021_11_26_053932) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer "recipient_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_124356) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
