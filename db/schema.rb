3dd5b593c90e5793c968cc288813a6a721466474
brahmakulkarni <brahma.kulkarni@iiitb.org>
2021-11-27 15:42:07 +0530

    Completed styling of post and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_053932) do
+ActiveRecord::Schema.define(version: 2021_11_26_063255) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "conversations", force: :cascade do |t|
     t.integer "recipient_id"
@@ -51,6 +61,8 @@ ActiveRecord::Schema.define(version: 2021_11_26_053932) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
 end
